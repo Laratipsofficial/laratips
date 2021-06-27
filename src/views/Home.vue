bd151321a648436808d25e01fbc88f3601981aa2
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2021-06-27 18:47:02 +0100

    add under construction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="h-screen flex flex-col items-center justify-center bg-primary lg:flex-row text-white">
-    <div>
-      <img src="/images/ashish-dhamala.jpg"
-           alt=""
-           class="w-64 h-64 lg:w-96 lg:h-96 rounded-full border-4 border-solid border-white">
-    </div>
+  <div class="h-screen flex flex-col bg-primary text-white">
+    <div class="text-center p-8 text-3xl lg:text-5xl">UNDER CONSTRUCTION!!!</div>
+    <div class="flex-1 flex flex-col lg:flex-row items-center justify-center">
+      <div>
+        <img src="/images/ashish-dhamala.jpg"
+            alt=""
+            class="w-64 h-64 lg:w-96 lg:h-96 rounded-full border-4 border-solid border-white">
+      </div>
 
-    <div class="mt-4 lg:mt-0 lg:ml-8">
-      <div class="text-4xl lg:text-6xl">Ashish Dhamala</div>
-      <div class="tracking-widest uppercase text-gray-200">Loves web development</div>
-      <div class="mt-4 flex items-center space-x-4">
-        <a href="https://www.youtube.com/c/laratips"
-           target="_blank">
-          <svgs name="youtube"
-                class="h-8 w-8 hover:text-gray-200" />
-        </a>
+      <div class="mt-4 lg:mt-0 lg:ml-8">
+        <div class="text-4xl lg:text-6xl">Ashish Dhamala</div>
+        <div class="tracking-widest uppercase text-gray-200">Loves web development</div>
+        <div class="mt-4 flex items-center space-x-4">
+          <a href="https://www.youtube.com/c/laratips"
+            target="_blank">
+            <svgs name="youtube"
+                  class="h-8 w-8 hover:text-gray-200" />
+          </a>
 
-        <a href="https://www.facebook.com/laratipsofficial"
-           target="_blank">
-          <svgs name="facebook"
-                class="h-8 w-8 hover:text-gray-200" />
-        </a>
+          <a href="https://www.facebook.com/laratipsofficial"
+            target="_blank">
+            <svgs name="facebook"
+                  class="h-8 w-8 hover:text-gray-200" />
+          </a>
+        </div>
       </div>
     </div>
   </div>
